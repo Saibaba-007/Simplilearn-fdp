ff36a0995ba8f3761ad1c1b0fb4196041060aa18
SAI BABA <SAI BABA@DESKTOP-RPRP4RM>
2020-12-16 11:28:20 +0530

    v4

--- calculator/src/calculator.java
+++ calculator/src/calculator.java
@@ -11,7 +11,7 @@ String digitButtonText[] = {"7", "8", "9", "4", "5", "6", "1", "2", "3", "0", "+
 String operatorButtonText[] = {"/", "sqrt", "*", "%", "-", "1/X", "+", "=" };  
 String memoryButtonText[] = {"MC", "MR", "MS", "M+" };  
 String specialButtonText[] = {"Backspc", "C", "CE" };  
-// test hnn
+// test 
   
 MyDigitButton digitButton[]=new MyDigitButton[digitButtonText.length];  
 MyOperatorButton operatorButton[]=new MyOperatorButton[operatorButtonText.length];  
